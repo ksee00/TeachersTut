5ea92c51d45bbdd0f4f83d201cb6aae606efbb9f
Koksin Developer <ksee00@hotmail.com>
2018-12-31 16:41:36 +0800

    Initial commit.. a> api/staffs ==> working b) api/stuents ==> not working due to serialization failure

---
+++ WebAppDemo/WebAppDemo/Controllers/StaffsController.cs
@@ -0,0 +1,22 @@
+﻿using SekolahDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAppDemo.Controllers
+{
+    public class StaffsController : ApiController
+    {
+        public IEnumerable<Staff> Get()
+        {
+            using (
+                SekolahDBEntities entities = new SekolahDBEntities())
+            {
+                return entities.Staffs.ToList();
+            }
+        }
+    }
+}
---
+++ WebAppDemo/WebAppDemo/Controllers/StudentsController.cs
@@ -0,0 +1,23 @@
+﻿using SekolahDataAccess;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Data.Entity;
+
+
+namespace WebAppDemo.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        public IEnumerable<Student> Get()
+        {
+            using (
+                
+                SekolahDBEntities entities = new SekolahDBEntities())
+            {
+                return entities.Students.ToList();
+            }
+        }
+
+    }
+}
